5116e832c6262432b8f8574ea67b67cce4eccdbf
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-04-24 13:46:49 +0200

    Another fix

--- utilities/visualize_keys.py
+++ utilities/visualize_keys.py
@@ -1,12 +1,18 @@
 import struct
+import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
-import matplotlib
 
-matplotlib.use('TkAgg')
+def clean_filename(filename):
+    # Remove _uint and everything that follows it
+    if "_uint" in filename:
+        filename = filename[:filename.index("_uint")]
 
+    # Remove file extension
+    filename = os.path.splitext(filename)[0]
+
+    return filename
 
 # Thanks @ ChatGPT for this script!
 
@@ -17,6 +23,7 @@ if len(sys.argv) < 2:
 
 # Define the file path
 file_path = sys.argv[1]
+clean_path = clean_filename(file_path)
 
 # Read the first 8 bytes (64 bits) of the file to get the number of keys
 with open(file_path, "rb") as f:
@@ -30,31 +37,10 @@ with open(file_path, "rb") as f:
     f.seek(8)  # Skip the first 8 bytes
     data = np.fromfile(f, dtype=np.uint32, count=num_keys)
 
-# Set up the plot
-fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.25)
+# Plot the key distribution
+plt.hist(data, bins=100)
 plt.title("Key Distribution")
 plt.xlabel("Key")
 plt.ylabel("Frequency")
 
-# Plot the initial histogram with 50 bins
-n_bins = 50
-hist, bins, _ = plt.hist(data, bins=n_bins)
-
-# Add a slider to adjust the number of bins
-axcolor = 'lightgoldenrodyellow'
-ax_slider = plt.axes([0.15, 0.1, 0.7, 0.03], facecolor=axcolor)
-slider = Slider(ax_slider, 'Number of Bins', valmin=1, valmax=1000, valinit=n_bins, valstep=1)
-
-# Function to update the histogram when the slider is changed
-def update(val):
-    global n_bins
-    n_bins = int(slider.val)
-    hist, bins = np.histogram(data, bins=n_bins)
-    for i, rectangle in enumerate(ax.patches):
-        rectangle.set_height(hist[i])
-    fig.canvas.draw_idle()
-
-slider.on_changed(update)
-
-plt.show(block=True)
+plt.savefig(clean_filename + "_distribution.jpg")
